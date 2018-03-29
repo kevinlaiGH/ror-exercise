d2dcce716b539266b2e65aaaf377fb380c97fa80
Kevin Lai <kevin.km.lai@gmail.com>
2018-03-29 15:32:28 +1100

    updated account balance

--- bankapp.rb
+++ bankapp.rb
@@ -18,3 +18,39 @@ class Customer
   userAddress = gets.chomp
   puts "what is your contact number?"
   userContact = gets.chomp
+
+
+class Bankaccount
+  def initialize(balance,accountnumber)
+    @balance = balance
+    @accountnumber = accountnumber
+ end
+attr_accessor :balance, :accountnumber
+ end
+
+trent = Customer.new("name", "email",  Bankaccount.new(200,1334), "address", "number" )
+trent.account.balance = trent.account.balance - 110
+
+
+def withdraw(withdraw)
+  @withdraw = withdraw
+end
+def balance(balance)
+  @balance = balance - @withdraw
+end
+print "Your balance is "
+puts  trent.account.balance
+puts "What file do you want to read?"
+filename = gets.chomp
+target = open(filename, 'a+')
+puts "Now please enter your withdraw: "
+withdraw = $stdin.gets.chomp
+puts "balance: 50 "
+balance =
+puts "Ok, thank you very much "
+target.write(balance)
+target.write("\n")
+target.rewind
+puts "=" * 15
+print target.read
+target.close
